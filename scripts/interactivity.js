a943ad8afa42fc4e3b39bc6649499f5b9071a33a
pablo <devpabloh@gmail.com>
2024-10-08 23:07:29 -0300

    att

--- scripts/interactivity.js
+++ scripts/interactivity.js
@@ -1,32 +1,41 @@
-document.getElementsByClassName("form").addEventListener('submit', function(event){
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const assunto = document.getElementById("assunto").value;
-    const message = document.getElementById("message").value
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.querySelector(".form");
 
-    if (name === "" || email === "" || assunto === "" || message === ""){
-        alert('Por favor, preencha todos os campos.')
-    }else{
-        alert('Mensagem enviada com sucesso!')
+    if(form){
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const assunto = document.getElementById("assunto").value;
+            const message = document.getElementById("message").value
+
+            if (name === "" || email === "" || assunto === "" || message === ""){
+                alert('Por favor, preencha todos os campos.')
+            }else{
+                alert('Mensagem enviada com sucesso!')
+            }
+        
+        });
     }
-
-})
+});
 
 document.addEventListener('DOMContentLoaded', function() {
-    const text = "Olá, meu nome é Pablo Henrique e construo páginas web.";
+    const text = "Olá, meu nome é Pablo Henrique e eu construo páginas web."; // Texto a ser digitado
+
+    console.log(text)
     const typingElement = document.getElementById("typing-effect");
     let index = 0;
 
     function type() {
         if (index < text.length) {
-            typingElement.textContent += text.charAt(index);
+            typingElement.textContent += text.charAt(index); // Adiciona um caractere
             index++;
-            setTimeout(type, 100); // Velocidade de digitação
+            setTimeout(type, 100); // Define a velocidade de digitação
         } else {
-            typingElement.innerHTML += `<span class="cursor"></span>`;
+            typingElement.innerHTML += `<span class="cursor"></span>`; // Adiciona o cursor no final
         }
     }
 
-    type();
+    type(); // Inicia a função de digitação
 });
